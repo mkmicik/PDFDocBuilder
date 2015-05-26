d161473a0323e496ad7f5509a860474ad1d4fa55
mkmicik <mkmicik@ualberta.ca>
2015-05-26 11:22:18 -0600

    Added Debenu PDF library and some skeleton code

---
+++ PDFDocBuilder/DAO/DataAccess.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace PDFDocBuilder.DAO
+{
+
+    public class DataAccess
+    {
+        private static DataAccess _instance;
+        private String conn_string;
+
+        private DataAccess()
+        {
+            conn_string = ConfigurationManager.ConnectionStrings["PDF_DATABASE"].ConnectionString;
+        }
+
+        public static DataAccess getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new DataAccess();
+            }
+            return _instance;
+        }
+
+        public DataTable getValues(int UID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] getPDFDoc(int UID)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
